69e40a1249a71cecd22306f4597cece8bdfb1983
Thierno Samb <thierno.samb@chakamobile.com>
2022-03-14 13:46:55 +0100

    initial commit

---
+++ src/main/java/com/example/MobiServices/MobiServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.MobiServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobiServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MobiServices/MobiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MobiServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
